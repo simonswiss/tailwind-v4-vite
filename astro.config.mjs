3fff14099d6c6e59c5ae5cb5fe130f0ee8fb83eb
Simon Vrachliotis <simon@simonswiss.com>
2024-03-07 06:51:05 +1100

    Adding Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
